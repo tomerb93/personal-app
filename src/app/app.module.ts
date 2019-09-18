9c10b23a5475d9f1f982d0dc01e264e6ff01f5a9
Tomer <tomerb93@gmail.com>
2019-09-18 11:49:12 +0300

    added code to finance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
-import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSidenavModule } from '@angular/material/sidenav';
 import {
   MatButtonModule,
   MatToolbarModule,
@@ -15,20 +15,21 @@ import {
   MatRippleModule,
   MatExpansionModule,
   MatInputModule,
-  MatGridListModule
-} from "@angular/material";
-import { MatCardModule } from "@angular/material/card";
-import { MatListModule } from "@angular/material/list";
+  MatGridListModule,
+  MatSelectModule
+} from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
-import { ToolbarComponent } from "./toolbar/toolbar.component";
-import { SideNavListComponent } from "./side-nav-list/side-nav-list.component";
-import { HomeComponent } from "./home/home.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { TasksListComponent } from "./tasks/tasks-list/tasks-list.component";
-import { TasksCreateComponent } from "./tasks/tasks-create/tasks-create.component";
-import { HttpClientModule } from "@angular/common/http";
-import { FinanceComponent } from "./finance/finance.component";
-import { ExpenseAddComponent } from "./finance/expense-add/expense-add.component";
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SideNavListComponent } from './side-nav-list/side-nav-list.component';
+import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TasksListComponent } from './tasks/tasks-list/tasks-list.component';
+import { TasksCreateComponent } from './tasks/tasks-create/tasks-create.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FinanceComponent } from './finance/finance.component';
+import { ExpenseAddComponent } from './finance/expense-add/expense-add.component';
 
 @NgModule({
   declarations: [
@@ -57,6 +58,7 @@ import { ExpenseAddComponent } from "./finance/expense-add/expense-add.component
     MatExpansionModule,
     MatInputModule,
     MatGridListModule,
+    MatSelectModule,
     FormsModule,
     ReactiveFormsModule
   ],
